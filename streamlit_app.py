81c66cb225b1b95df57f78e4016a324338a02d66
veronikaspa <134385474+veronikaspa@users.noreply.github.com>
2023-05-23 15:43:25 +0200

    Update streamlit_app.py
    Kale, Spinach & Rocket Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.title('Breakfast Menu')
 streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
