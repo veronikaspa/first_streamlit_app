9c977d1d658c78a6f0b75ad8f310906f1f74c048
veronikaspa <134385474+veronikaspa@users.noreply.github.com>
2023-05-23 15:54:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
 streamlit.title('Breakfast Menu')
 streamlit.title('Omega 3 and Blueberry Oatmeal')
 streamlit.title('Kale, Spinach & Rocket Smoothie')
